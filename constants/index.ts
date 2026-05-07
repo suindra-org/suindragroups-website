766daf24f027fcad6124f78a643755dfaa5f4260
Mohit Shahi <moheetshahi92@gmail.com>
2026-05-07 19:46:16 +0545

    chore: remove mohit

--- constants/index.ts
+++ constants/index.ts
@@ -1,54 +1,49 @@
 // constants/index.ts
 
 export const NAV_LINKS = [
-    { label: 'Home', href: '/' },
-    { label: 'Services', href: '#services' },
-    { label: 'Ventures', href: '#ventures' },
-    { label: 'About', href: '#about' },
-  ];
-  
-  export const PRODUCTS = [
-    {
-      id: "buynow",
-      title: "BuyNow",
-      description: "The ultimate marketplace for secure digital purchases and reliable transactions.",
-      link: "https://buynow.suindragroups.com.np/",
-      status: "Live",
-      platform: "Web & Mobile App"
-    },
-    {
-      id: "sellnow",
-      title: "SellNow",
-      description: "Empowering local businesses with a seamless selling platform. Streamlining trade in Nepal.",
-      link: "#", // Dummy for now
-      status: "Launching Soon",
-      platform: "Mobile App"
-    }
-  ];
+  { label: "Home", href: "/" },
+  { label: "Services", href: "#services" },
+  { label: "Ventures", href: "#ventures" },
+  { label: "About", href: "#about" },
+];
+
+export const PRODUCTS = [
+  {
+    id: "buynow",
+    title: "BuyNow",
+    description:
+      "The ultimate marketplace for secure digital purchases and reliable transactions.",
+    link: "https://buynow.suindragroups.com.np/",
+    status: "Live",
+    platform: "Web & Mobile App",
+  },
+  {
+    id: "sellnow",
+    title: "SellNow",
+    description:
+      "Empowering local businesses with a seamless selling platform. Streamlining trade in Nepal.",
+    link: "#", // Dummy for now
+    status: "Launching Soon",
+    platform: "Mobile App",
+  },
+];
 
 export const TEAM_MEMBERS = [
-    { 
-      name: "Ravi Pajiyar", 
-      role: "Technical Head & Full Stack Developer", 
-      education: "Computer Engineer",
-      image: "/raviprofile.jpeg", 
-      linkedin: "https://linkedin.com", 
-      portfolio: "https://portfolio.com" 
-    },
-    { 
-      name: "Mohit Shahi", 
-      role: "Full Stack Developer", 
-      education: "Computer Engineer",
-      image: "/mohitprofile.jpeg", 
-      linkedin: "https://linkedin.com", 
-      portfolio: "https://portfolio.com" 
-    },
-    { 
-      name: "Ashwani Chaudhary", 
-      role: "Junior Software Developer", 
-      education: "Computer Engineer",
-      image: "/ashwaniprofile.png", 
-      linkedin: "https://linkedin.com", 
-      portfolio: "https://portfolio.com" 
-    },
-  ];
+  {
+    name: "Ravi Pajiyar",
+    role: "Technical Head & Full Stack Developer",
+    education: "Computer Engineer",
+    image: "/raviprofile.jpeg",
+    linkedin: "https://linkedin.com",
+    portfolio: "https://portfolio.com",
+  },
+
+  {
+    name: "Ashwani Chaudhary",
+    role: "Junior Software Developer",
+    education: "Computer Engineer",
+    image: "/ashwaniprofile.png",
+    linkedin: "https://linkedin.com",
+    portfolio: "https://portfolio.com",
+  },
+];
